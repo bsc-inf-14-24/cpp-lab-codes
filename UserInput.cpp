b0b0a69448a32b35d9723b3853289f50d3e1fc81
bsc-inf-14-24 <bsc-inf-14-24@unima.ac.mw>
2026-04-25 07:14:28 +0200

    Initial commit - C++ programs

---
+++ UserInput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x, y;
+    int sum;
+
+    cout << "Type a number: ";
+    cin >> x;
+
+    cout << "Type another number: ";
+    cin >> y;
+
+    sum = x + y;   // fixed here
+    cout << "Sum is: " << sum;
+
+    return 0;
+}
